c27454a705a57d56a833fb4668336ebb512b5a31
Teddy Ghim <teddyghim@teddyui-macbook-pro.home>
2018-04-26 16:18:40 +0200

    course 9-11

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,30 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/03/15/08/the-matrix-reboot-0.jpg",
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFjEu58aCQZhk6XQnSXWnHtu5irZAU5pyIkUupwqhv0mFZZ4H1YQ",
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  },
-  {
-    title: "Star Wars",
-    poster: "http://fr.web.img6.acsta.net/pictures/17/10/10/04/58/2061488.jpg",
-  }
-]
-
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  
+  state ={
+    greeting: 'Hello!',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/03/15/08/the-matrix-reboot-0.jpg",
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFjEu58aCQZhk6XQnSXWnHtu5irZAU5pyIkUupwqhv0mFZZ4H1YQ",
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+      },
+      {
+        title: "Star Wars",
+        poster: "http://fr.web.img6.acsta.net/pictures/17/10/10/04/58/2061488.jpg",
+      }
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Star Wars",
+            poster: "http://fr.web.img6.acsta.net/pictures/17/10/10/04/58/2061488.jpg",
+          },
+          ...this.state.movies,
+        ]
+      })
+    } , 1000)
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
