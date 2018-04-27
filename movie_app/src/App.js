1406d05eb1a4a155b9f53557d00f6876fcd4aac6
Teddy Ghim <teddyghim@gmail.com>
2018-04-28 01:12:45 +0200

    course 13~14

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -6,39 +6,39 @@ class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
   
-  state ={
-  }
+  state ={ }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/03/15/08/the-matrix-reboot-0.jpg",
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFjEu58aCQZhk6XQnSXWnHtu5irZAU5pyIkUupwqhv0mFZZ4H1YQ",
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-          },
-          {
-            title: "Star Wars",
-            poster: "http://fr.web.img6.acsta.net/pictures/17/10/10/04/58/2061488.jpg",
-          }
-        ]
-      })
-    } , 1000)
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'))
+    // setTimeout(() => {
+    //   this.setState({
+    //     movies: [
+    //       {
+    //         title: "Matrix",
+    //         poster: "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/03/15/08/the-matrix-reboot-0.jpg",
+    //       },
+    //       {
+    //         title: "Full Metal Jacket",
+    //         poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFjEu58aCQZhk6XQnSXWnHtu5irZAU5pyIkUupwqhv0mFZZ4H1YQ",
+    //       },
+    //       {
+    //         title: "Oldboy",
+    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+    //       },
+    //       {
+    //         title: "Star Wars",
+    //         poster: "http://fr.web.img6.acsta.net/pictures/17/10/10/04/58/2061488.jpg",
+    //       }
+    //     ]
+    //   })
+    // } , 1000)
   }
 
   _runderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
       return <Movie title={movie.title} poster={movie.poster} key={index} />
     })
-    return movies;
+    return movies
   }
 
   render() {
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,27 +2,52 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
+// class Movie extends Component{
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <div className="poster-title">{this.props.title}</div>
-            </div>
-        )
-    }
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <div className="poster-title">{this.props.title}</div>
+//             </div>
+//         )
+//     }
+// }
+
+// class MoviePoster extends Component{
+//     render(){
+//         return(
+//             <img className="lv-boots-img" src={this.props.poster} />
+//         )
+//     }
+// }
+
+// Smart & Dumb Components
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+function MoviePoster({poster}){
+    return(
+        <img src={poster} />
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img className="lv-boots-img" src={this.props.poster} />
-        )
-    }
+Movie.prototype = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
+
+MoviePoster.prototype = {
+    poster: PropTypes.string.isRequired
+}
+
 export default Movie;
