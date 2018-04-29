7f7e1d24829a02b70afb4bfc609f41820cea2381
Teddy Ghim <teddyghim@gmail.com>
2018-04-29 10:14:40 +0200

    finished

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -20,7 +20,11 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie title={movie.title} 
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                    synopsis={movie.synopsis}
+                    key={movie.id} />
     })
     return movies
   }
@@ -41,9 +45,10 @@ class App extends Component {
   }
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App-loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -27,27 +27,59 @@ import './Movie.css';
 // }
 
 // Smart & Dumb Components
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return(
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div classNmae="Movie">
+            <div className="Movie__Columns">
+            <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Moive__Genres">
+                    {genres.map((genre,index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Moive__Synopsis">
+                    <MovieSynopsis synopsis={synopsis}/>
+                </div>
+            </div>
         </div>
     )
 }
-function MoviePoster({poster}){
+function MoviePoster({poster,alt}){
     return(
-        <img src={poster} />
+        <img className="Movie__Poster" src={poster} title={alt} alt={alt} />
+    )
+}
+function MovieGenre({genre}){
+    return(
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+function MovieSynopsis({synopsis}){
+    return(
+        <span className="Movie__Synopsis">{synopsis}</span>
     )
 }
 
 Movie.prototype = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.prototype = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
+
+MovieGenre.prototype = {
+    genre: PropTypes.string.isRequired
+}
+
+MovieSynopsis.prototype = {
+    synopsis: PropTypes.string.isRequired
 }
 
 export default Movie;
